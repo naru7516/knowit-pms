50eb35d557c3b0a85441840c74f764026953acd8
Ajay Tushar Narkhede <136894927+naru7516@users.noreply.github.com>
2023-10-31 23:59:29 +0530

    Update ClassComp.js

--- ClassComp.js
+++ ClassComp.js
@@ -3,7 +3,7 @@ import React from "react";
 class ClassComp extends React.Component
 {
     render(){
-        return <h1>Hello from class comp</h1>
+        return <h1>Hello from class component</h1>
     }
 }
 export default ClassComp;
